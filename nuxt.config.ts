c6e00ad679409831d4fbe054041b1e37a121bfab
Posoroko <eric@posoroko.com>
2023-05-15 13:15:51 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,31 @@ export default defineNuxtConfig({
                     type: 'image/x-icon',
                     href: '/images/site-icon.png',
                 },
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/favicon/apple-touch-icon.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '32x32',
+                    href: '/favicon/favicon-32x32.png',
+                },
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    sizes: '16x16',
+                    href: '/favicon/favicon-16x16.png',
+                },
+                {
+                    rel: "manifest",
+                    href: "/favicon/site.webmanifest"
+                },
+                {
+                    rel: "shortcut icon",
+                    href: "/favicon/favicon.ico"
+                }
             ],
             charset: 'utf-8',
             viewport: 'width=500, initial-scale=1',
